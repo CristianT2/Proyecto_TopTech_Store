75491c9b4b0190c4e5d23a0f82352841319c9b6c
Cristian Torrejon <c.torrejon96@gmail.com>
2022-08-03 16:34:10 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Proyecto_TopTech_Store
---
+++ src/test/java/com/toptech/store/ProyectoTopTechStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toptech.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTopTechStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
